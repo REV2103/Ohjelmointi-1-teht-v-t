e22cc4bc990e6ebd57bc960afe15442e39daafdd
ronim <roni.minkkinen@gmail.com>
2025-01-14 22:47:09 +0200

    Tein tehtävän 4

---
+++ Moduuli 2/Tehtava4.py
@@ -0,0 +1,12 @@
+luku_1_str = input("Anna ensimmäinen luku ")
+luku_2_str = input("Anna toinen luku ")
+luku_3_str = input("Anna kolmas luku ")
+luku_1 = float(luku_1_str)
+luku_2 = float(luku_2_str)
+luku_3 = float(luku_3_str)
+summa = luku_1+luku_2+luku_3
+tulo = luku_1*luku_2*luku_3
+ka = summa/3
+print(f"Lukujen summa: {summa}")
+print(f"Lukujen tulo: {tulo}")
+print (f"Lukujen keskiarvo: {ka}")
