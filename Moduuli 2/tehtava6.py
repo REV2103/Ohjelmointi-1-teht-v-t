c0d9a0a36ed64dec4b4bda4cbcc85f66a91c6a1e
Ronimin <roni.minkkinen@gmail.com>
2025-01-16 11:37:39 +0200

    Tein tehtävän 5

---
+++ Moduuli 2/tehtava6.py
@@ -0,0 +1,11 @@
+import random
+
+r1 = random.randint(0, 9)
+r2 = random.randint(0, 9)
+r3 = random.randint(0, 9)
+print(f"Satunnainen kolminumeroinen koodi: {r1}{r2}{r3}")
+r5 = random.randint(1, 6)
+r6 = random.randint(1, 6)
+r7 = random.randint(1, 6)
+r8 = random.randint(1, 6)
+print(f"Satunnainen nelinumeroinen koodi: {r5}{r6}{r7}{r8}")
