9a1ff34d611373692b3596237e0544e402271a43
Ronimin <roni.minkkinen@gmail.com>
2025-01-16 11:26:34 +0200

    Tein tehtävän 5

--- Moduuli 2/Tehtava5.py
+++ Moduuli 2/Tehtava5.py
@@ -9,6 +9,6 @@ n_g = naulat*32*13.3
 lei_g = leiviskat*20*32*13.3
 summa_grammoina = luo_g+n_g+lei_g
 sumkg = summa_grammoina//1000
-print(f"Summa nykymittojen mukaan: {sumkg:12.0f} kilogrammaa {summa_grammoina%1000:12.2f} grammaa")
+print(f"Massa nykymittojen mukaan:{sumkg:12.0f} kilogrammaa {summa_grammoina%1000:12.2f} grammaa")
 
 
