a11ec8f328090ca6c8112efaa9229e396e6d6831
Ronimin <roni.minkkinen@gmail.com>
2025-01-28 15:20:12 +0200

    Tein tehtävän 4

---
+++ Moduuli 5/Tehtava 4.py
@@ -0,0 +1,7 @@
+kaupunkilista = []
+for luku in range(5):
+    kaupunki = input("Anna kaupungin nimi ")
+    kaupunkilista.append(kaupunki)
+print("Antamien kaupunkiesi nimet: ")
+for kaupunki in kaupunkilista:
+    print(kaupunki)
