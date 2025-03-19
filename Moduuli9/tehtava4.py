3d4e14b6a903f67c326f080429eed82cf585de50
rev2103 <roni.minkkinen@gmail.com>
2025-03-19 13:15:40 +0200

    Tehtävä 4 tehty. Pitää selvittää miten saada looppi keskeytettyä kun yksi auto saavuttaa 10k km

---
+++ Moduuli9/tehtava4.py
@@ -0,0 +1,36 @@
+import random
+from tabulate import tabulate
+class Auto:
+    def __init__(self, rekisteritunnus, huippunopeus, th_nopeus, matka):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.th_nopeus = th_nopeus
+        self.matka = matka
+    def kiihdytä(self, kiihdytys):
+        self.th_nopeus = self.th_nopeus+int(kiihdytys)
+        if self.th_nopeus > self.huippunopeus:
+            self.th_nopeus = self.huippunopeus
+        return self.th_nopeus
+    def kulje(self, aika):
+        self.matka = aika*self.th_nopeus
+        global ktk
+        ktk = False
+        if self.matka >= 10000:
+            ktk = True
+        return self.matka, ktk
+autolista = []
+taulukon_tunnisteet = ["Rekisteritunnus", "Huippunopeus", "Tämänhetkinen Nopeus", "Kuljettu Matka"]
+for i in range(10):
+    autolista.append(Auto(f"ABC-{1+i}", random.randint(100, 200), 0, 0))
+aika = 0
+for auto in autolista:
+    while True:
+        aika = aika + 1
+        auto.kiihdytä(random.randint(-10, 15))
+        auto.kulje(aika)
+        if ktk == True:
+            break
+if ktk == True:
+    print(taulukon_tunnisteet)
+    for auto in autolista:
+        print(f"{auto.rekisteritunnus}, {auto.huippunopeus} km/h, {auto.th_nopeus} km/h, {auto.matka} km")
