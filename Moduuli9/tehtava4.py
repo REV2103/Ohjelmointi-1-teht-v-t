00acedf5cb2af27cec69b008000304fb59751e98
ronim <roni.minkkinen@gmail.com>
2025-03-21 00:35:40 +0200

    Tehtävä 4 korjattu

--- Moduuli9/tehtava4.py
+++ Moduuli9/tehtava4.py
@@ -1,5 +1,4 @@
 import random
-from tabulate import tabulate
 class Auto:
     def __init__(self, rekisteritunnus, huippunopeus, th_nopeus, matka):
         self.rekisteritunnus = rekisteritunnus
@@ -12,24 +11,19 @@ class Auto:
             self.th_nopeus = self.huippunopeus
         return self.th_nopeus
     def kulje(self, aika):
-        self.matka = aika*self.th_nopeus
-        global ktk
-        ktk = False
-        if self.matka >= 10000:
-            ktk = True
-        return self.matka, ktk
+        self.matka += aika * self.th_nopeus
+        return self.matka
 autolista = []
 taulukon_tunnisteet = ["Rekisteritunnus", "Huippunopeus", "Tämänhetkinen Nopeus", "Kuljettu Matka"]
 for i in range(10):
     autolista.append(Auto(f"ABC-{1+i}", random.randint(100, 200), 0, 0))
-aika = 0
-for auto in autolista:
-    while auto.matka <= 10000:
-        auto.kiihdytä(random.randint(-10, 15))
-        auto.kulje(aika)
-        aika = aika + 1
-    if auto.matka <= 10000:
-        break
+ktk = False
+while not ktk:
+    for auto in autolista:
+            auto.kiihdytä(random.randint(-10, 15))
+            auto.kulje(1)
+            if auto.matka >= 10000:
+                ktk = True
 print(taulukon_tunnisteet)
 for auto in autolista:
     print(f"{auto.rekisteritunnus}, {auto.huippunopeus} km/h, {auto.th_nopeus} km/h, {auto.matka} km")
